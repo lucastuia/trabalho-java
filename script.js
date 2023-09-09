6a9fc2cdd8ebf07e6e04534d7db67fdb1eee7cf0
Lucas O.Gonçalves <134063912+lucastuia@users.noreply.github.com>
2023-09-08 21:48:52 -0300

    trabalho java lucas

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document) .ready(function(){
+    //adicionar o código javascript - jquery dentro do document.ready
+    $('#btnClique').click(function(){
+       //acao do clique no elemento definido
+       alert('');
+
+       //variavel
+       var nome=prompt('Digite seu nome');
+       alert(nome);
+       $('mensagem').html('<div class="alert alert-warning" role="alert">'+nome+'</div>');
+    });
+});
+
+$(document) .ready(function(){
+    //adicionar o código javascript - jquery dentro do document.ready
+    $('#btnClique').click(function(){
+       //acao do clique no elemento definido
+       alert('dffddddcd');
+
+       //variavel
+       var nome=prompt('Digite seu nome');
+       alert(nome);
+       $('jonas').html('<div class="alert alert-warning" role="alert">'+nome+'</div>');
+    });
+});
