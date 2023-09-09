2bd2ccb9d59d7285bb76d7c7c4df66d928974ecf
Lucas O.Gonçalves <134063912+lucastuia@users.noreply.github.com>
2023-09-09 17:57:32 -0300

    trabalho java-script lucas terminar

--- script.js
+++ script.js
@@ -1,45 +1,90 @@
 $(document) .ready(function(){
     
     function CienciasHumanas() {
-        var escolha = prompt("Selecione a matéria");
+        var escolha = prompt("Selecione a matéria (letra minúscula)");
 
         switch (escolha.toLowerCase()) { 
-            case "História":
-                alert("Matéria: História. Este é o texto de História:")
+            case "história":
+                alert("texto de História:");
                 break;
-            case "Geografia":
-                alert("Matéria: Geografia. Este é o texto de Geografia:")
+            case "geografia":
+                alert("texto de Geografia:");
                 break;
-            case "Filosofia":
-                alert("Matéria: Filosofia. Este é o texto de Filosofia:")
+            case "filosofia":
+                alert("texto de Filosofia:");
                 break;    
-            case "Sociologia":
-                alert("Matéria: Sociologia. Este é o texto de Sociologia:")
+            case "sociologia":
+                alert("texto de Sociologia:");
                 break; 
             default:
-                alert("Isso não existe ou a matéria foi escrita errada.")
+                alert("Isso não existe ou a matéria foi escrita errada.");
                 break;    
         }
     }
-
-
     var botaoMateria = document.getElementById("btn_ch");
     botaoMateria.addEventListener("click", CienciasHumanas);
+
+
+    
+    function CienciasDaNatureza() {
+        var escolha = prompt("Selecione a matéria (letra minúscula)");
+
+        switch (escolha.toLowerCase()) { 
+            case "física":
+                alert("texto de Física:");
+                break;
+            case "biologia":
+                alert("texto de Biologia:");
+                break;
+            case "química":
+                alert("texto de Química:");
+                break;    
+            default:
+                alert("Isso não existe ou a matéria foi escrita errada.");
+                break;    
+        }
+    }
+    var botaoMateria = document.getElementById("btn_cn");
+    botaoMateria.addEventListener("click", CienciasDaNatureza);
+
+
+    
+    function Linguagens() {
+        var escolha = prompt("Selecione a matéria (letra minúscula)");
+
+        switch (escolha.toLowerCase()) { 
+            case "artes":
+                alert("texto de Artes:");
+                break;    
+            case "inglês":
+                alert("texto de Inglês:");
+                break;
+            case "português":
+                alert("texto de Português:");
+                break;    
+            default:
+                alert("Isso não existe ou a matéria foi escrita errada.");
+                break;    
+        }
+    }
+    var botaoMateria = document.getElementById("btn_ling");
+    botaoMateria.addEventListener("click", Linguagens);
+    
     
     $("#btn_mat") .click(function(){
-        alert("teste3")
+        alert("teste1")
      });
     
     $("#btn_tec") .click(function(){
-       alert("teste4")
+       alert("teste2")
     });
 
     $("#btn_pv") .click(function(){
-        alert("teste5")
+        alert("teste3")
      });
 
      $("#btn_pp") .click(function(){
-        alert("teste6")
+        alert("teste4")
      });
 
 
